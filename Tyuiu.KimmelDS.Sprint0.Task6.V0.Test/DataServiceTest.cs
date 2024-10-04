fd72ddd4373385ca1010593721b3040851ec2a51
EvilHui <Ukrumoski@gmail.com>
2024-10-04 18:16:47 +0500

    s6

---
+++ Tyuiu.KimmelDS.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KimmelDS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KimmelDS.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] {1, 2, 3, 4, 5};
+            var result = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var result = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, result);
+        }
+
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var result = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, result);
+        }
+    }
+}
