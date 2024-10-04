85c448a5247de3460c5f0f1fb770255afd25c8ff
EvilHui <Ukrumoski@gmail.com>
2024-10-04 16:33:01 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KimmelDS.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Tyuiu.KimmelDS.Sprint0.Task3.V0.Lib
+{
+    public class DataService 
+    {
+        //Реализация метода Sum
+        public static int Sum(int a, int b) => a + b;
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.KimmelDS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KimmelDS.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
